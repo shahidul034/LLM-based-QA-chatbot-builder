114709a1352ed49a99d791d8f2275be7b725edae
Shahidul Shakib <31099713+shahidul034@users.noreply.github.com>
2024-04-19 00:41:48 +0600

    added

--- UI/deploy/main.py
+++ UI/deploy/main.py
@@ -1,12 +1,16 @@
 import gradio as gr
+from inference_deploy import rag_chain_ret
+from inference_deploy import ans_ret 
 with gr.Blocks() as demo:
     with gr.Tab("Inference"):
+            f=open("deploy//info.txt","r").read().split("\n")
+            username=f[0]
+            model_name=f[1]
+            rag_chain=rag_chain_ret(f"{username}/{model_name}")
             def echo(message, history,model_name):
                 gr.Info("Please wait!!! Model is loading!!")
-                #$$$$$$$$$$$$$$$$$
-                # rag_chain=rag_chain_ret("My_model")
                 # return ans_ret(message,rag_chain)
-                return "Llama"
-            
+                return "message"
+
             gr.ChatInterface(fn=echo, title="My chatbot")
 demo.launch(share=False)
